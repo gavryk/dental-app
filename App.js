6b26128dd2fe1f61d0bf27c186050587357d6064
gvozdyk <gvozdyk23@gmail.com>
2021-11-24 20:42:14 +0200

    first steps

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import styled from 'styled-components/native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Group>
+        <GroupTitle>11 December</GroupTitle>
+        <GroupItem>
+          <Avatar source={{uri: 'https://365psd.com/images/istock/previews/1009/100996291-male-avatar-profile-picture-vector.jpg' }}></Avatar>
+          <FullName>Angelo Migos</FullName>
+        </GroupItem>          
+      </Group>      
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const FullName = styled.Text`
+
+`;
+
+const Avatar = styled.Image`
+  border-radius: 50px;
+  width: 50px;
+  height: 50px;
+`;
+
+const GroupItem = styled.View`
+  padding: 10px 0;
+`;
+
+const GroupTitle = styled.Text`
+  font-weight: 800;    
+  font-size: 22px;
+  color: #000;
+  text-align: left;
+  margin-bottom: 15px;
+`;
+
+const Group = styled.View`
+  padding: 0 15px;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  margin: 50px 0;
+`; 
