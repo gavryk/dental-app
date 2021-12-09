ff5f2516d49acae32cbf551cb64ea3b08d7f8639
gvozdyk <gvozdyk23@gmail.com>
2021-12-09 20:45:23 +0200

    section list onRefresh fetch appointments

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { RefreshControl, SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 // import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -8,23 +7,13 @@ import { HomeScreen, PatientScreen } from "./screens";
 const Stack = createNativeStackNavigator();
 // const Drawer = createDrawerNavigator();
 
-const wait = (timeout) => {
-  return new Promise((resolve) => setTimeout(resolve, timeout));
-};
 
 const App = () => {
-  const [refreshing, setRefreshing] = React.useState(false);
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
     wait(2000).then(() => setRefreshing(false));
   }, []);
-<ScrollView
-  contentContainerStyle={styles.scrollView}
-  refreshControl={
-    <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-  }
-></ScrollView>;
 
   return (
     <NavigationContainer>
@@ -36,14 +25,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    flex: 1,
-    backgroundColor: "pink",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
