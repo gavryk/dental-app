d82cf2eb5bd00a88be4e1ab0c4347ed8fa9c8a64
gvozdyk <gvozdyk23@gmail.com>
2021-12-09 20:37:43 +0200

    set color in avatar, refresh

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import React from "react";
-import styled from "styled-components";
-import { SafeAreaView, SafeAreaProvider, Text, Button } from "react-native";
+import { RefreshControl, SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+// import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen, PatientScreen } from "./screens";
 
 const Stack = createNativeStackNavigator();
 // const Drawer = createDrawerNavigator();
 
+const wait = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
+
 const App = () => {
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+<ScrollView
+  contentContainerStyle={styles.scrollView}
+  refreshControl={
+    <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+  }
+></ScrollView>;
+
   return (
     <NavigationContainer>
       {/* <SafeAreaView style={{ backgroundColor: '#2A86FF' }}/> */}
@@ -20,5 +36,14 @@ const App = () => {
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default App;
--- utils/getAvatarColor.js
+++ utils/getAvatarColor.js
@@ -1,18 +1,18 @@
 export default (letter) => {
   const charCode = letter.charCodeAt();
-  if (charCode >= 1048 && charCode <= 1055) {
+  if (charCode >= 64 && charCode <= 73) {
     return {
       background: "#F5D6D9",
       color: "#F38181",
     };
   }
-  if (charCode >= 1049 && charCode <= 1063) {
+  if (charCode >= 74 && charCode <= 82) {
     return {
       background: "#F8ECD5",
       color: "#F1A32F",
     };
   }
-  if (charCode >= 1064 && charCode <= 1071) {
+  if (charCode >= 83 && charCode <= 90) {
     return {
       background: "#DAD5F8",
       color: "#816CFF",
